00d23b5ea4b9046dc33052d6d13aa761b4022284
Nata <natayafs@gmail.com>
2020-02-11 22:35:57 -0300

    second commit

--- ejercicio3/Program.cs
+++ ejercicio3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace ejercicio3
 {
@@ -6,7 +9,38 @@ namespace ejercicio3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Dado un valor numérico entero m, 
+            determinar e imprimir un listado con los m primeros 
+            múltiplos de 3 que no sean múltiplos de 5*/
+
+            //Ingresa el valor
+            Console.WriteLine("Ingrese un numero entero m: ");
+            int m;
+            m = int.Parse(Console.ReadLine());
+            
+            //Calcula los multiplos y los agrega a las listas
+            List<int> Lista1 = new List<int>();
+            List<int> Lista2 = new List<int>();
+
+            for (int i = 0; i <= m; i++)
+        
+                if (i % 3 == 0)
+                {
+                    Lista1.Add(i);
+                }
+
+            for (int j = 0; j <= m; j++)
+    
+                if (j % 5 == 0)
+                {
+                    Lista2.Add(j);
+                }
+            
+            //Imprime el listado final
+            var ListaFinal = Lista1.Except(Lista2);
+            Console.WriteLine("Los multiplos de 3, pero no de 5 son: ");
+            Console.WriteLine(ListaFinal.Take(m));
+
         }
     }
 }
